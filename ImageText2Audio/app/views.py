5bdc35c3ed34068488fbe576e18ccf90f462c830
Jason <jasonkrithik@gmail.com>
2022-02-22 11:11:38 +0530

    Added basic structure

--- ImageText2Audio/app/views.py
+++ ImageText2Audio/app/views.py
@@ -1,12 +1,63 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from flask_appbuilder import ModelView
 from flask_appbuilder.models.mongoengine.interface import MongoEngineInterface
 from app import appbuilder
+import os
+from werkzeug.utils import secure_filename
+
+def allowed_image(filename):
+
+    # We only want files with a . in the filename
+    if not "." in filename:
+        return False
+
+    # Split the extension from the filename
+    ext = filename.rsplit(".", 1)[1]
+
+    # Check if the extension is in ALLOWED_IMAGE_EXTENSIONS
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
 
 """
     Define you Views here
 """
 
+app.config["IMAGE_UPLOADS"] = "./uploads"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            if image.filename == "":
+                print("No filename")
+                return redirect(request.url)
+
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+                print("Image saved")
+
+                return redirect(request.url)
+
+            else:
+                print("That file extension is not allowed")
+                return redirect(request.url)
+
+    return render_template("upload_image.html")
 
 """
     Application wide 404 error handler
